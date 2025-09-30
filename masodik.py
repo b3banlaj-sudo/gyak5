de0ce83e8ff49e593433356bd3c7b287c320c6c4
b3banlaj-sudo <b3banlaj@vasvari.org>
2025-09-30 14:50:20 +0200

    masodik

---
+++ masodik.py
@@ -0,0 +1,8 @@
+a = float(input("A téglalap egyik oldala: "))
+b = float(input("A téglalap másik oldala: "))
+
+terulet = a *b
+kerulet = 2  * (a+b)
+
+print(f"A téglalap kerülete: {kerulet}")
+print(f"A téglalap területe: {terulet}")
